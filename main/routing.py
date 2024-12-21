08f16da69704b25852919f8ce5faa013706be288
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-12-21 14:51:45 +0530

    made it wss

--- main/routing.py
+++ main/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import ChatConsumerDemo
 
 websocket_urlpatterns = [
-  path("ws/ai-demo/", ChatConsumerDemo.as_asgi()),
+  path("wss/ai-demo/", ChatConsumerDemo.as_asgi()),
 ]
